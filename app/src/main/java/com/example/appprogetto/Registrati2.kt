57f547241624c436ea6b7cbe3961a8f61ff4e636
IsabellaMarini <76102845+IsabellaMarini@users.noreply.github.com>
2022-08-21 12:09:18 +0200

    Modifiche registrazione 2

---
+++ app/src/main/java/com/example/appprogetto/Registrati2.kt
@@ -0,0 +1,13 @@
+package com.example.appprogetto
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Registrati2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pagina_accesso)
+        val intent: Intent = getIntent()
+    }
+}
--- app/src/main/java/com/example/appprogetto/paginaRegistrati.kt
+++ app/src/main/java/com/example/appprogetto/paginaRegistrati.kt
@@ -2,13 +2,19 @@ package com.example.appprogetto
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class paginaRegistrati : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.pagina_registrati)
+        setContentView(R.layout.pagina1_registrati)
         val intent : Intent = getIntent()
+        var avanti = findViewById<Button>(R.id.avanti)
+            avanti.setOnClickListener() {
+                val intent = Intent(this, Registrati2::class.java)
+                startActivity(intent)
+            }
     }
 
 
