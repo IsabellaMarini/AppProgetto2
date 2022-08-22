5a55a9ed9b27033083e80239b6bb7ae1a502a59c
IsabellaMarini <76102845+IsabellaMarini@users.noreply.github.com>
2022-08-22 16:03:14 +0200

    Login parziale

---
+++ app/src/main/java/com/example/appprogetto/Utenti.kt
@@ -0,0 +1,3 @@
+package com.example.appprogetto
+
+data class Utenti(val nome : String?=null , val cognome : String?=null, val username  : String?=null, val email : String?=null, val password : String?=null, val data_nascita : String?=null)
--- app/src/main/java/com/example/appprogetto/paginaAccesso.kt
+++ app/src/main/java/com/example/appprogetto/paginaAccesso.kt
@@ -3,19 +3,37 @@ package com.example.appprogetto
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.example.appprogetto.databinding.ActivityMainBinding
+import com.example.appprogetto.databinding.PaginaAccessoBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class paginaAccesso : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        var bindig : PaginaAccessoBinding
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pagina_accesso)
         val intent : Intent = getIntent()
-
         var indietro= findViewById<Button>(R.id.indietro3)
         indietro.setOnClickListener() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
+        var accedi = findViewById<Button>(R.id.Accedi2)
+        var username2 = findViewById<EditText>(R.id.email)
+        var password2 = findViewById<EditText>(R.id.Password)
+        val database:DatabaseReference = FirebaseDatabase.getInstance().getReference("users")
+        val User = Utenti()
+        
+
+            accedi.setOnClickListener (){
+                val intent = Intent(this, Home::class.java)
+                startActivity(intent)
+            }
     }
 
 
