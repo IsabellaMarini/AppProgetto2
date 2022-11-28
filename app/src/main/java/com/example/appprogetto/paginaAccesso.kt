a7e0afbfab51184fdb6ea3848f6c7c5b1e6130e9
AldoPaganica <75017659+AldoPaganica@users.noreply.github.com>
2022-11-28 14:45:01 +0100

    login google

--- app/src/main/java/com/example/appprogetto/paginaAccesso.kt
+++ app/src/main/java/com/example/appprogetto/paginaAccesso.kt
@@ -31,7 +31,9 @@ class paginaAccesso : AppCompatActivity() {
     private lateinit var binding: PaginaAccessoBinding
     private lateinit var user: FirebaseAuth
 
-    private lateinit var googleSignInClient: GoogleSignInClient
+
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    val Req_Code:Int=123
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,21 +42,23 @@ class paginaAccesso : AppCompatActivity() {
         binding = PaginaAccessoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        user = FirebaseAuth.getInstance()
-
-        checkIfUserIsLogged()
-
-
-        val GoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        // Configure Google Sign In inside onCreate mentod
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
-        googleSignInClient = GoogleSignIn.getClient(this, GoogleSignInOptions)
+        // getting the value of gso inside the GoogleSigninClient
+        mGoogleSignInClient=GoogleSignIn.getClient(this,gso)
+        // initialize the firebaseAuth variable
+        user = FirebaseAuth.getInstance()
 
 
-        findViewById<Button>(R.id.google_connect).setOnClickListener() {
+        checkIfUserIsLogged()
+
+        findViewById<Button>(R.id.google_connect).setOnClickListener(){
             signInGoogle()
         }
+
 
 
         var indietro = findViewById<Button>(R.id.indietro3)
@@ -117,47 +121,58 @@ class paginaAccesso : AppCompatActivity() {
     }
 
 
-    private fun signInGoogle() {
-        val signInIntent = googleSignInClient.signInIntent
-        launcher.launch(signInIntent)
+    // signInGoogle() function
+    private  fun signInGoogle(){
 
+        val signInIntent:Intent=mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent,Req_Code)
+    }
+    // onActivityResult() function : this is where we provide the task and data for the Google Account
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==Req_Code){
+            val task:Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleResult(task)
+        }
     }
 
-    private val launcher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-
-                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-                handleResults(task)
-
-            }
-        }
-
-    private fun handleResults(task: Task<GoogleSignInAccount>) {
-        if (task.isSuccessful) {
-            val account: GoogleSignInAccount? = task.result
+    // handleResult() function -  this is where we update the UI after Google signin takes place
+    private fun handleResult(completedTask: Task<GoogleSignInAccount>){
+        try {
+            val account: GoogleSignInAccount? =completedTask.getResult(ApiException::class.java)
             if (account != null) {
-                updateUi(account)
+                UpdateUI(account)
             }
-
-        } else {
-            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+        } catch (e:ApiException){
+            Toast.makeText(this,e.toString(),Toast.LENGTH_SHORT).show()
         }
-
     }
-    private fun updateUi(account: GoogleSignInAccount) {
-        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
-        user.signInWithCredential(credential).addOnCompleteListener {
-            if(it.isSuccessful){
-                val intent : Intent = Intent(this, pagina_home::class.java)
+
+    // UpdateUI() function - this is where we specify what UI updation are needed after google signin has taken place.
+    private fun UpdateUI(account: GoogleSignInAccount){
+        val credential= GoogleAuthProvider.getCredential(account.idToken,null)
+        user.signInWithCredential(credential).addOnCompleteListener {task->
+            if(task.isSuccessful) {
+                SavedPreference.setEmail(this,account.email.toString())
+                SavedPreference.setUsername(this,account.displayName.toString())
+                val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
-
-            } else {
-                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                finish()
             }
         }
-
     }
+    override fun onStart() {
+        super.onStart()
+        if(GoogleSignIn.getLastSignedInAccount(this)!=null){
+            startActivity(Intent(this, pagina_home::class.java))
+            finish()
+        }
+    }
+
+
+
+
+
 
 }
 
